81476a55822efcfb1e06181226dcab49674b2f41
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-06-01 00:06:01 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,83 @@
+const express = require("express")
+const server = express()
+const routes = require("./routes")
+const path = require("path")
+
+const port= normalizePort(process.env.PORT || 3000);//normalizando porta
+server.set('port',  port)// definindo porta
+
+// usando template engine
+server.set('view engine',  'ejs')
+
+// Mudar a localização da pasta views
+server.set('views', path.join(__dirname, 'views'))
+
+//habilitar arquivos statics
+server.use(express.static("public"))
+
+// usar o req.body
+server.use(express.urlencoded({ extended: true }))
+
+// Habilita o CORS
+server.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+// routes
+server.use(routes)
+
+//console.clear(); 
+// aponta para a aporta onde devera rodar o servidor 
+server.listen(port, () => console.log(`Servidor rodando em (${__dirname}) em http://localhost:${port}`));
+
+// onError
+server.on('error', onError);
+// onListenig
+server.on('listening', onListening);
+// function abaixo
+// tratando erros
+function onError(error) {
+  if(error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port ==='string' ? 
+    'Pipe ' + port :
+    'Port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+        console.error(bind + ' requires elevated privilegies.' );
+        process.exit(1);
+    case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+    default:
+        throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug(`Listening on ${bind}`);
+}
+// ----
+function normalizePort(val){
+    const port = parseInt(val, 10);
+    if(isNaN(port)){
+        return val;
+    }
+    if(port >= 0){
+        return port;
+    }
+    return false;
+}
+
+
+// diretorio do servidor
+//console.log(`Server dir: `+ __dirname);
+
+// para rodar o servidor com nodemon
+// npm run dev
